5211a140e1ea2f63df07631dd18cc3fd98c47bf4
swati <swatirajput8077@gmail.com>
2026-05-01 13:35:36 +0530

    Updated Main.java

--- src/main/java/org/example/Demo/Main.java
+++ src/main/java/org/example/Demo/Main.java
@@ -5,6 +5,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+//this is main file
 public class Main {
     public static void main(String[] args) {
 //                ApplicationContext context=
