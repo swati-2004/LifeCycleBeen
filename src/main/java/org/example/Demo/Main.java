6ee53a38389af1f776327c7788bf82ba7e49b080
swati <swatirajput8077@gmail.com>
2026-05-01 14:09:59 +0530

    Added comment in featur branch

--- src/main/java/org/example/Demo/Main.java
+++ src/main/java/org/example/Demo/Main.java
@@ -7,6 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 //this is main file
 //createing a main file
+// This is a comment added in feature branch
 public class Main {
     public static void main(String[] args) {
 //                ApplicationContext context=
